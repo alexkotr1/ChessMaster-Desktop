6a70349131b91f2310e802305589766a180e7887
alexk <alexkotrotsios123@gmail.com>
2024-12-26 16:45:08 +0200

    remove some unused dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,5 @@
 module com.alexk.chessgui {
-    requires javafx.fxml;
     requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
     requires java.desktop;
-    requires javafx.media;
-
-    opens com.alexk.chess to javafx.fxml;
     exports com.alexk.chess;
 }
